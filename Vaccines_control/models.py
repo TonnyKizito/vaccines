2e1231021e843c8cbb60f0e8d810bd957d26b0c4
KIZITO TONNY <116958469+TonnyKizito@users.noreply.github.com>
2022-11-08 18:05:28 +0300

    Update models.py

--- Vaccines_control/models.py
+++ Vaccines_control/models.py
@@ -16,12 +16,27 @@ class Health_Facility(models.Model):
 		return self.name
 
 
+
+
+vaccines_cat = (
+		('BCG','BCG'),('DPT-HepB-Hib','DPT-HepB-Hib'),('OPV','OPV'),('IPV','IPV'),
+        ('Rotavirus vaccine','Rotavirus vaccine'),
+        ('Yellow Fever','Yellow Fever'),
+        ('Measles Rubella','Measles Rubella'),('PCV','PCV'),('HPV','HPV'),('Tetanus Toxiod diptheria(Td)','Tetanus Toxiod diptheria(Td)'),('0.05mls','0.05mls'),('0.5mls','0.5mls'),('2mls','2mls'),
+        
+        ('5mls','5mls'),('0.1mls','0.1mls'),('Mabendazole','Mabendazole'),('Vitamin A 200,000iu','Vitamin A 200,000iu'),
+        ('Vitamin A 100,000iu','Vitamin A 100,000iu'),('Albendazole','Albendazole'),
+
+        )
+
+
 class Stock(models.Model):
         health_facility = models.ForeignKey(Health_Facility, on_delete=models.CASCADE,blank=True)
         health_facility = models.CharField(max_length=50, blank=True, null=True,)
         district= models.CharField(max_length=50,null=True,)
-#       category = models.CharField(max_length=50, blank=True, null=True,choices=category_choice)
-        vaccine_name = models.CharField(max_length=50, blank=True, null=True)
+        Date=models.DateField(auto_now=True)
+        vaccine_name = models.CharField(max_length=50, blank=True, null=True,choices=vaccines_cat)
+        # vaccine_name = models.CharField(max_length=50, blank=True, null=True)
         quantity = models.IntegerField(default='0', blank=True, null=True)
         vial_size = models.CharField(max_length=50, blank=True, null=True)
         receive_quantity = models.IntegerField(default='0', blank=True, null=True)
@@ -220,3 +235,29 @@ class Patient(models.Model):
         return self.user.first_name+" ("+self.symptoms+")"
 
 
+
+
+
+
+class Issue(models.Model):
+        health_facility = models.ForeignKey(Health_Facility, on_delete=models.CASCADE,blank=True)
+        health_facility = models.CharField(max_length=50, blank=True, null=True,)
+        district= models.CharField(max_length=50,null=True,)
+        Date=models.DateField(auto_now=True)
+        vaccine_name = models.CharField(max_length=50, blank=True, null=True,choices=vaccines_cat)
+       
+        issue_quantity = models.IntegerField(default='0', blank=True, null=True)
+        # issue_quantity = models.ForeignKey(Stock, on_delete=models.CASCADE, blank=True, null=True)
+        issue_by = models.CharField(max_length=50, blank=True, null=True)
+        issue_to = models.CharField(max_length=50, blank=True, null=True)
+        doses_given_to_other_facilities = models.IntegerField(default='0', blank=True, null=True)
+        number_vaccinated = models.IntegerField(default='0', blank=True, null=True)
+        
+        last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+        timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+        
+        ##date = models.DateTimeField(auto_now_add=False, auto_now=False)
+        ##export_to_CSV = models.BooleanField(default=False)
+        def __str__(self):
+                return str(self.vaccine_name) +' - '+ str(self.health_facility)
+                
